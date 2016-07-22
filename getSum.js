3d487db83e6649d94e7200b683d1ae36dab3e2ca
David Andelin <iAndelin@outlook.com>
2016-07-21 18:58:47 -0600

    Create getSum.js
    Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.

---
+++ getSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+        if(a === 0){
+            return b;
+        }
+        if(b === 0){
+            return a;
+        }
+        let sum = a ^ b;
+        let carry = (a & b) <<1;
+        return getSum(sum, carry);
+};
+
+console.log(getSum(4, 95));
